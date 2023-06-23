61d539593e3dbdd8fa9fafd1bd9a08ca873c0c72
raytech99 <raymanatl@gmail.com>
2023-06-23 19:13:44 -0400

    Altered some syntax for readability

--- lab_assignment_6.c
+++ lab_assignment_6.c
@@ -4,19 +4,23 @@
 int search(int numbers[], int low, int high, int value) 
 {
 	
-    if (low > high){
+    if (low > high)
+	{
         return -1;  
     }
     
     int mid = low + (high - low) / 2; 
     
-    if (numbers[mid] == value){
+    if (numbers[mid] == value)
+	{
         return mid; 
     } 
-	else if (numbers[mid] > value){
+	else if (numbers[mid] > value)
+	{
         return search(numbers, low, mid - 1, value); 
     } 
-	else{
+	else
+	{
         return search(numbers, mid + 1, high, value); 
     }
 
